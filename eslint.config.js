1a5aa492b517b6f4ff4bff826f4a7ef3acc9ba32
AntonioMrtz <antonio@metrica-sports.com>
2026-02-01 17:32:03 +0100

    chore: fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,19 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import typescriptParser from "@typescript-eslint/parser";
+
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  },
   {
     rules: {
       // override/add rules settings here, such as:
