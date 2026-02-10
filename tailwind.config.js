b22b4aa24c91406d42530afd7ba8e789caf5a0ac
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2026-02-10 15:34:49 +0100

    feat: add tailwind support for custom colors in text-[x] classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,31 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        accent: {
+          500: "var(--accent-orange-500)",
+        },
+        "secondary-purple": {
+          300: "var(--secondary-purple-300)",
+          400: "var(--secondary-purple-400)",
+          500: "var(--secondary-purple-500)",
+          600: "var(--secondary-purple-600)",
+          900: "var(--secondary-purple-900)",
+        },
+        "primary-black": {
+          400: "var(--primary-black-400)",
+          500: "var(--primary-black-500)",
+          600: "var(--primary-black-600)",
+        },
+        neutral: {
+          white: "var(--neutral-white)",
+          gray: {
+            50: "var(--neutral-gray-50)",
+            100: "var(--neutral-gray-100)",
+            500: "var(--neutral-gray-500)",
+          },
+        },
+      },
       fontFamily: {
         sans: ["Ubuntu", "system-ui", "sans-serif"],
       },
