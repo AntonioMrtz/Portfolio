83bdf4fc588f1c96199f99dbff6a68529cd54515
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2026-02-10 16:29:53 +0100

    feat: add languages in education section

--- src/data/education.ts
+++ src/data/education.ts
@@ -1,8 +1,9 @@
 import umuIcon from "@/assets/icons/education/umu.svg";
+import languagesIcon from "@/assets/icons/education/languages.svg";
 
 export { type EducationID, type Education, EDUCATION_ID_TO_DATA };
 
-type EducationID = "cs-degree-umu";
+type EducationID = "cs-degree-umu" | "languages";
 
 interface Education {
   id: EducationID;
@@ -10,7 +11,7 @@ interface Education {
   iconUrl: ImageMetadata;
   entity: string;
   description: string;
-  dates: string;
+  dates?: string;
 }
 
 const EDUCATION_ID_TO_DATA: Readonly<Record<EducationID, Education>> = {
@@ -22,5 +23,12 @@ const EDUCATION_ID_TO_DATA: Readonly<Record<EducationID, Education>> = {
       "- Grade: 7.5/10 (GPA 3.3/4.0)\n- Software Engineering Specialization\n- Software Architecture with Honors",
     dates: "2019 - 2023",
     entity: "University of Murcia",
+  },
+  languages: {
+    id: "languages",
+    label: "Languages",
+    iconUrl: languagesIcon,
+    description: "- 🇪🇸 Spanish: Native\n- 🇬🇧 English: C1 (Advanced)",
+    entity: "Personal",
   },
 };
