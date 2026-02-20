37c52a3d4589ff281cab5d47546c94d9e6387022
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2026-02-20 13:15:33 +0100

    ci: fix linting pipeline

--- eslint.config.js
+++ eslint.config.js
@@ -2,21 +2,22 @@ import eslintPluginAstro from "eslint-plugin-astro";
 import typescriptParser from "@typescript-eslint/parser";
 
 export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
-  ...eslintPluginAstro.configs.recommended,
   {
-    files: ["src/**/*.ts"],
+    files: ["**/*.ts", "**/*.astro"],
     languageOptions: {
       parser: typescriptParser,
       parserOptions: {
         project: "./tsconfig.json",
+        extraFileExtensions: [".astro"],
       },
     },
   },
+
+  ...eslintPluginAstro.configs.recommended,
+
+  // global overrides or custom rules
   {
     rules: {
-      // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
     },
   },
