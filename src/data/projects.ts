f7edb3f3629f52f5282c46d8ac6d30e040f100a3
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2026-05-31 20:25:46 +0200

    feat: add portfolio to projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,10 +2,11 @@ import type { ImageMetadata } from "astro";
 import type { TechId } from "./stack";
 import adventjsIcon from "@/assets/images/companies/adventjs.webp";
 import adventjs2024Description from "@/data/files/project-descriptions/adventjs-2024.txt?raw";
+import portfolioIcon from "@/assets/images/me/logoAM.webp";
 
 export { type ProjectId, PROJECT_ID_TO_DATA, type Project };
 
-type ProjectId = "adventjs-2024";
+type ProjectId = "adventjs-2024" | "portfolio";
 
 interface Project {
   id: ProjectId;
@@ -26,5 +27,15 @@ const PROJECT_ID_TO_DATA: Readonly<Record<ProjectId, Project>> = {
     repoUrl: "https://github.com/AntonioMrtz/AdventJS-2024",
     iconUrl: adventjsIcon,
     tech: ["typescript", "python"],
+  },
+  portfolio: {
+    id: "portfolio",
+    name: "Portfolio",
+    description:
+      "My personal portfolio website built with Astro, showcasing my projects and skills.",
+    websiteUrl: "https://antoniomrtz.github.io/Portfolio/",
+    repoUrl: "https://github.com/AntonioMrtz/Portfolio",
+    iconUrl: portfolioIcon,
+    tech: ["astro", "typescript", "css"],
   },
 };
